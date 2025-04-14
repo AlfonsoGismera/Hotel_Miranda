06b1f8e7cac9b494e29069b70355d2369c312396
Alfonso <alfonsofcogismeraperea@gmail.com>
2025-04-14 23:06:41 +0200

    Act: Header and Footer

--- js/main.js
+++ js/main.js
@@ -1,15 +1,11 @@
-// main.js
 document.addEventListener('DOMContentLoaded', () => {
     const toggleBtn = document.querySelector('.header__toggle');
     const nav = document.querySelector('.header__nav');
     let menuOpen = false;
   
     toggleBtn.addEventListener('click', () => {
-      // Alternamos la clase que controla el despliegue del menú
       nav.classList.toggle('header__nav--active');
       menuOpen = !menuOpen;
-      
-      // También puedes cambiar el ícono entre "hamburguesa" y "X"
       if (menuOpen) {
         toggleBtn.innerHTML = '<i class="fas fa-times"></i>';
       } else {
