7c500e4fdce4a15bfb4a86cd6cdc8ea8e6b466b3
Alfonso <alfonsofcogismeraperea@gmail.com>
2025-04-17 11:22:45 +0200

    Add: Core_White

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 import hero_dates from "./hero.js";
 import initHeaderToggle from "./header.js";
 import initSwiperRooms from "./slider.js";
+import initCoreFeaturesSwiper from "./sliderFeatures.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   hero_dates();
   initHeaderToggle();
   initSwiperRooms();
+  initCoreFeaturesSwiper();
 });
